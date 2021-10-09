1cf00dd3da15ced262bec3f9b45b7ad9520cd38d
Ruchir Saxena <ruchirsaxena.coder@gmail.com>
2021-10-09 18:09:42 +0530

    Using Use Context Hook

--- src/components/ComponentB.jsx
+++ src/components/ComponentB.jsx
@@ -1,9 +1,15 @@
 import ComponentC from "./ComponentC";
+import { useContext } from "react";
+import { UserContext, ProductContext } from "../App";
 
 export default function ComponentB() {
+  const user = useContext(UserContext);
+  const Product = useContext(ProductContext);
   return (
     <div className="App">
       <h4>Hello from Component B</h4>
+      <div>Using useContext</div>
+      {user} - {Product}
       <ComponentC />
     </div>
   );
--- src/components/ComponentC.jsx
+++ src/components/ComponentC.jsx
@@ -1,25 +1,26 @@
-import { UserContext, ProductContext } from '../App';
+import { UserContext, ProductContext } from "../App";
 
 export default function ComponentC() {
   return (
     <div className="App">
       <h4>Hello from Component C</h4>
-       <UserContext.Consumer>
-         {
-           (value) => {
-             return (
-               <ProductContext.Consumer>
-                 {
-                   product => {
-                   return <div> User Contest value {value}, Product Context Value {product}</div>
-                   }
-                 }
-               </ProductContext.Consumer>
-                
-             )
-           }
-         }
-       </UserContext.Consumer>
+      <div>Using Provider - Old Way</div>
+      <UserContext.Consumer>
+        {(value) => {
+          return (
+            <ProductContext.Consumer>
+              {(product) => {
+                return (
+                  <div>
+                    {" "}
+                    {value} - {product}
+                  </div>
+                );
+              }}
+            </ProductContext.Consumer>
+          );
+        }}
+      </UserContext.Consumer>
     </div>
   );
 }
